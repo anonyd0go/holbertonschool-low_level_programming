d4bf26a8cb4c0c885f3b1144f57697a77b12c111
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-21 16:40:57 -0400

    Correct program to print a string using write

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	/*int size_s = sizeof(s);*/
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int size_s = sizeof(s) - 1;
 
-	write(2, s, 59);
+	write(2, s, size_s);
 
 	return (1);
 }
