cd83bcb0ec25d3de0c9d78f1538159b44fa4cab7
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-22 13:22:42 -0400

    Add program that prints out ascii without char datatype

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print single digit number combination
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ascii = 49;
+
+	while (ascii < 57)
+	{
+		putchar(ascii);
+		putchar(',');
+		putchar(' ');
+		ascii++;
+	}
+	putchar(ascii);
+
+	return (0);
+}
