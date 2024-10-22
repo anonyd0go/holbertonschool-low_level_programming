e836472ca9c60258057ba9dddb8b8b6c9d27d86f
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-22 10:17:23 -0400

    Add program that determines the value of the last number

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Determines the last digit of a random number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int lst_d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	lst_d = n % 10;
+	printf("The last digit of %i is ", n);
+	if (lst_d > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (lst_d == 0)
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
+	return (0);
+}
