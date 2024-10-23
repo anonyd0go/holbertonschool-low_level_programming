045d4b041ddad4050363d24fef17f0db2fbea6db
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-23 14:36:56 -0400

    Add function def to find if char is alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is alphabetical
+ * @c: Character to view
+ * Return: 1 if alphabetical, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ('A' <= c && c <= 'z')
+		return 1;
+
+	return 0;
+}
