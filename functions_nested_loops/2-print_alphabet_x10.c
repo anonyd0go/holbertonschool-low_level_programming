c94b4544fd45a53ec9bfe6459891e6022a0a9b61
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-23 13:32:08 -0400

    Add prog that prints alph 10x

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints the alphabet 10 times
+ * Return: Always void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char a;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (a = 'a'; a < 'z' ; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
