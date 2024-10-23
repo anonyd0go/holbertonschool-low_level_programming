c10333974c52a4721715ddb694d9c60ce39b301b
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-23 13:55:13 -0400

    Add function that checks if a char is lowwercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - determines if a char is lowercase
+ * @c: Character used to find if lowercase or upper
+ * Return: 1 if char is lower case, 0 otherwise
+ */
+int _islower(int c)
+{
+	int confirmation;
+
+	if ('a' <= c && c <= 'z')
+		confirmation = 1;
+	if ('A' <= c && c <= 'Z')
+		confirmation = 0;
+
+	return (confirmation);
+}
