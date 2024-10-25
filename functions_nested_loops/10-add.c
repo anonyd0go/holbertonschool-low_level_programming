9dcfaca8bcd1d11f998744678447c902644fe1ce
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-25 09:28:27 -0400

    Add function that returns sum of number

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: one of the integers to sum
+ * @n: other integer tu sum
+ * Return: Result of the sum of the digits
+ */
+int add(int i, int n)
+{
+	return (i + n);
+}
