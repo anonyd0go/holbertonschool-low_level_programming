5c796501da26d7affb4dd27a70f6be14d723cb9d
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-26 09:19:26 -0400

    Add funct that prints sum of mults of 3 and 5

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the sum of the numbers that are multiples of 3 and 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	int sum;
+	int target = 1024;
+	int i;
+
+	for (i = 0; i < target; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
