b1bcfc7c0a63d51fc244cfdb3c4308c89a5bdb5d
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-27 15:11:22 -0400

    Add function that prints 98 fibonacci nums

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci nums
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long int first = 1;
+	unsigned long int second = 2;
+	int limit = 96;
+	int i;
+	unsigned long int fibo;
+
+	printf("%lu, %lu, ", first, second);
+	for (i = 0; i < limit; i++)
+	{
+		fibo = first + second;
+		first = second;
+		second = fibo;
+		if (i < (limit - 1))
+			printf("%lu, ", fibo);
+		else
+			printf("%lu\n", fibo);
+	}
+
+	return (0);
+}
