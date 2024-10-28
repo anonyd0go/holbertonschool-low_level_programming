4f82f39ec8fd8b5e3ca00b69529d6fa5b6d03033
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-28 10:23:22 -0400

    Add function that returns the mult of two digits

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two ints
+ * @a: first int
+ * @b: second int
+ * Return: Returns the multiplication of two ints
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
