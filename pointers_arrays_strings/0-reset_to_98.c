60630fcfc675c33bc34f5ddf9ff2e5d1602d046c
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-29 12:50:13 -0400

    Add function that changes the value at an addrs

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of an int to 98
+ * @n: pointer to the int that will change
+ * Return: Always void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
