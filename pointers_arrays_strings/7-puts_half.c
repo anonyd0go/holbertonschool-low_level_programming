f83860e3831fce4afe0ea0695f028d382eddb0f3
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-30 11:17:48 -0400

    Correct function that prints half the str

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 	for (ln = 0; *(vis + ln) != '\0'; ln++)
 	{}
 
-	for (vis = str + (ln /2); vis < str + ln; vis++)
+	for (vis = str + (ln / 2); vis < str + ln; vis++)
 	{
 		_putchar(*vis);
 	}
