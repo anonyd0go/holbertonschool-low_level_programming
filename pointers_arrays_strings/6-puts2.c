1b92bbf84aa977d41b2c8ad633d295838a154cb0
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-30 09:35:35 -0400

    Add func that prints every other char

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char on a str
+ * @str: string to be printed
+ * Return: Void
+ */
+void puts2(char *str)
+{
+	char *prn;
+
+	for (prn = str; *prn != '\0'; prn++)
+	{
+		_putchar(*prn);
+		prn++;
+	}
+	_putchar('\n');
+}
