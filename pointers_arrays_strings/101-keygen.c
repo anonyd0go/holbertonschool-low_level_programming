e5a9110e6f1117cf4c707b92f34cf0c58fddf653
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-10-31 15:21:50 -0400

    Correct funtion that generates a random password

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -12,16 +12,13 @@ int main(void)
 	int upper = 126;
 	unsigned int pkln = 11;
 	unsigned int i;
-	char passkey[pkln];
 
 	srand(time(NULL));
 
 	for (i = 0; i <= pkln; i++)
 	{
-		passkey[i] = rand() % (upper - lower) + lower;
+		printf("%c", rand() % (upper - lower) + lower);
 	}
-
-	printf("%s", passkey);
 
 	return (0);
 }
