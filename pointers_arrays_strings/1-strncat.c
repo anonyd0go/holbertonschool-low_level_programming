608616e3286c43b08a2440e9531ed95672f62eca
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-01 09:55:38 -0400

    Add function that concatinates two strs to n elements

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatinates two strings until N chars
+ * @dest: Where both strs will be concatinated
+ * @src: Str that will be added
+ * Return: Pointer to the concatinated str
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int d_ln = 0;
+	int s_ln = 0;
+	int i;
+
+	while (dest[d_ln] != '\0')
+		d_ln++;
+
+	while(src[s_ln] != '\0')
+		s_ln++;
+
+	if (n > s_ln)
+		n = s_ln;
+	if (n < 0)
+		n = 0;
+
+	for (i = 0; i < n; i++, d_ln++)
+		dest[d_ln] = src[i];
+
+	dest[d_ln + 1] = '\0';
+
+	return (dest);
+}
