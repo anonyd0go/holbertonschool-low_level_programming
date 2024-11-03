24576624f0700f8d1ffb0447f265e8509ac2d6b7
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-03 13:32:49 -0400

    Correct rot13 function

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,18 @@ char *rot13(char *s)
 {
 	int i;
 	int j;
-	char *in = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *ot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char ot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; in[j] != '\0'; j++)
 		{
 			if (s[i] == in[j])
+			{
 				s[i] = ot[j];
+				break;
+			}
 		}
 	}
 
