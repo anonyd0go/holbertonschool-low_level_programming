730d3dae9af245d9ff6addf24106ebd09f65fe45
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-03 11:09:19 -0400

    Correct function that prints ints

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -10,11 +10,21 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	if (n < 0)
+	{
+		if (n / 10)
+			print_number(n / -10);
+	}
+	else
+	{
+		if (n / 10)
+			print_number(n / 10);
+	}
 
-	_putchar('0' + (n % 10));
+	if (n < 0)
+		_putchar('0' + ((n % 10) * -1));
+	else
+		_putchar('0' + (n % 10));
 }
