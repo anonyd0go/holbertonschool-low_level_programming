0d632d9b6b405cbd0da05a83052081d1751565cb
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-04 13:22:40 -0400

    Add function that copies n bytes of data

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy: copies n bytes form memory of src to dest
+ * @dest: Destination of copied data
+ * @src: Starting point of data to be copied
+ * @n: Size of data to copy
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
