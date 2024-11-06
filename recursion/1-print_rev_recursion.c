86ba2e748c333425e6ad00e881d9fbf29119e23d
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-06 10:48:02 -0400

    Add function that prints a str in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Str to print
+ * Return: Void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*(s + 1) != '\0')
+		_print_rev_recursion(s + 1);
+
+	_putchar(*s);
+}
