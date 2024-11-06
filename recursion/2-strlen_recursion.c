0ef153a007e5c0d2248dd7ca88a3eac838001e40
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-06 11:24:47 -0400

    Add function that gets the len of a str

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Finds the length of a str
+ * @s: Str to find its len
+ * Return: Size of the str in int
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return(0);
+	else if (*s == '\0')
+		return (1);
+
+	return (1 + _strlen_recursion(s + 1));
+}
