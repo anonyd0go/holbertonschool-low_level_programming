eff8bd5ecc27e5b9433c639a34cd235198231b65
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-07 12:30:34 -0400

    Add function that prints the sum of numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Prints the sum of positive numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 if sum is succesfull and all arguments are digits, else 1
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+
+}
