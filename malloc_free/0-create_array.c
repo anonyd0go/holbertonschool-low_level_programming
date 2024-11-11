6f263d4ee8126f938458f548f144bd409f7c410a
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-11 19:29:45 -0400

    Correct function that creates a str

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,12 +15,13 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
-	s = malloc(size);
+	s = malloc(size * sizeof(char));
 	if (s == NULL)
 		return (0);
 
-	for (i = 0; i <= size; i++)
+	for (i = 0; i < size; i++)
 		s[i] = c;
+	s[size] = '\0';
 
 	return (s);
 }
