cc3a96070759e5fc464f0ece3f8fe7777ad344cd
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-11 12:40:08 -0400

    Add function that dynamically creates a str

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array and initializes it
+ * @size: Size of the array
+ * @c: Character to initiallize the array
+ * Return: ptr to new array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+
+	s = malloc(size);
+	if (s == NULL)
+		return (0);
+
+	for (i = 0; i <= size; i++)
+		s[i] = c;
+
+	return (s);
+}
