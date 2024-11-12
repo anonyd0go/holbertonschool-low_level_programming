64f094dc2fe5cbdfcf691435b7014282367be794
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-11 20:20:46 -0400

    Add custom strdup function

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+unsigned int _strlen(char *str);
+/**
+ * _strdup - Copyes a str into allocated memory
+ * @str: String to be copied
+ * Return: Pointer to copied str or null if there is an error
+ */
+char *_strdup(char *str)
+{
+	unsigned int size, i;
+	char *cp;
+
+	if (!str || str == NULL)
+		return (0);
+
+	size = _strlen(str);
+	cp = malloc(size * sizeof(char));
+
+	if (cp == NULL)
+		return (0);
+
+	for (i = 0; i <= size; i++)
+		*(cp + i) = *(str + i);
+
+	return (cp);
+}
+
+
+/**
+ * _strlen - Finds the length of a str
+ * @str: string to find the length
+ * Return: an unsigned int with the size of the str
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	if (str)
+	{
+		for (i = 0; str[i]; i++)
+			;
+
+		return (i);
+	}
+
+	return (0);
+}
