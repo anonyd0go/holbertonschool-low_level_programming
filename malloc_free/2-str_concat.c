04dcf911825903655d2669376aa1ebb3c08066a0
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-12 09:39:47 -0400

    Add function that concatinates two str in dynamic alloc mem

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+unsigned int _strlen(char *str);
+
+/**
+ * str_concat - concatinates two strs
+ * @s1: One of the strs to concatinate
+ * @s2: Second str to concatinate
+ * Return: a pointer to new memory of concatinanted strs
+ */
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, len1, len2;
+	char *concat;
+
+	if (s1 == 0 && s2 == 0)
+		return (0);
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	concat = malloc((len1 + len2) * sizeof(char));
+	if (concat == NULL)
+		return (0);
+
+	if (len1)
+	{
+		for (i = 0; i < len1; i++)
+			concat[i] = s1[i];
+	}
+	if (len2)
+	{
+		for (i = 0; i < len2; i++)
+			concat[len1 + i] = s2[i];
+	}
+
+	return (concat);
+}
+
+
+/**
+ * _strlen - Custom strlen function
+ * @str:  String we want the length of
+ * Return: Length of the str or 0 if empty
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int len = 0;
+
+	if (!str)
+		return (0);
+
+	while (*(str + len))
+		len++;
+
+	return (len);
+}
