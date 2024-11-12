e881e6c4844a15355fc9c2b08404d41ccca5c4fb
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-12 11:27:22 -0400

    Add function that dynamically allocates memory for a grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Dynamically allocates memory for a 2D array
+ * @width: Number of columns
+ * @height: Number of rows
+ * Return: Pointer to 2D array of ints
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grd;
+	int i, j;
+
+	if (width < 1 || height < 1)
+		return (0);
+
+	grd = (int **) malloc((size_t)height * sizeof(int *));
+	if (grd == NULL)
+		return (0);
+	for (i = 0; i < height; i++)
+	{
+		grd[i] = (int *)malloc((size_t)width * sizeof(int));
+		if (grd[i] == NULL)
+			return (0);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			grd[i][j] = 0;
+	}
+
+	return (grd);
+}
