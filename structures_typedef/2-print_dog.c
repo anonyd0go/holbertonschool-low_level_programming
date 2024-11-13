feddb55658ab68979c03a2a8c10926105e6cf8f8
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-13 16:47:50 -0400

    Add function that prints struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - Prints the struct dog
+ * @d: Pointer to the dog that will be printed
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)");
+
+	if (d->age)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)");
+
+	if (d->owner)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)");
+}
