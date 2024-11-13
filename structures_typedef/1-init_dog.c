85d736945a880c0832b71d8b9586d8429bc193d1
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-13 16:26:39 -0400

    Add function to initialize dog struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a var of type struct dog
+ * @d: Ptr to struct dog var
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Return: Void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
