cb9d61a566b740ad21ed42cd3962816131e9f628
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-14 09:45:41 -0400

    Correct function to create a struct dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
+unsigned int _strlen(char *str);
+char *_strcpy(char *str, unsigned int ln);
 
 /**
  * new_dog - Creates a dog struct in memory
@@ -16,9 +18,55 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr == NULL)
 		return (0);
 
-	ptr->name = name;
+	ptr->name = _strcpy(name, _strlen(name));
+	if (ptr->name == NULL)
+		return (0);
+
 	ptr->age = age;
-	ptr->owner = owner;
+
+	ptr->owner = _strcpy(owner, _strlen(owner));
+	if (ptr->owner == NULL)
+		return (0);
 
 	return (ptr);
+}
+
+
+/**
+ * _strlen - Finds the length of a str
+ * @str: String to analyze
+ * Return: length of the str or 0 if empty or NULL
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	if (!str || str == NULL)
+		return (0);
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+
+/**
+ * _strcpy - Copies a str to a new destination
+ * @str: String to copy
+ * @ln: Length of the string
+ * Return: pointer to the copy of the str, NULL otherwise
+ */
+char *_strcpy(char *str, unsigned int ln)
+{
+	unsigned int i;
+	char *cp;
+
+	cp = malloc((ln + 1) * sizeof(char));
+	if (cp == NULL)
+		return (0);
+
+	for (i = 0; i < ln; i++)
+		cp[i] = str[i];
+	cp[ln] = '\0';
+
+	return (cp);
 }
