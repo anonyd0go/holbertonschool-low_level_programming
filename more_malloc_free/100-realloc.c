0150afdefa156bbed61bacc3aa00480fe5ed602a
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-15 10:33:53 -0400

    Add custom realloc function

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocate a memory block
+ * @ptr: Memory to resize
+ * @old_size: Old size of memory
+ * @new_size: New size of memory
+ * Return: Ptr to modified memory, NULL if it fails
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	unsigned char *bfr, *new;
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (0);
+		return (ptr);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	bfr = ptr;
+	new = malloc(new_size);
+	if (new == NULL)
+		return (0);
+
+	if (new_size == old_size)
+	{
+		free(new);
+		return (ptr);
+	}
+	else if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			new[i] = bfr[i];
+	}
+	else
+	{
+		for (i = 0; i < new_size; i++)
+			new[i] = bfr[i];
+	}
+
+	free(ptr);
+	return (new);
+}
