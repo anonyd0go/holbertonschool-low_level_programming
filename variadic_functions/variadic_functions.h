1e494a21fd94b6aaf1c454bf5441c3f2505da42d
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-11-21 10:00:38 -0400

    Update custom headder file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,15 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+/**
+ * struct print_format - organizes respective print functions
+ * @format: Character that rapresents the desired format
+ * @func: Function pertaining to the desired format
+ */
+typedef struct print_format
+{
+char *format;
+void (*func)(va_list);
+} tprnt;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
