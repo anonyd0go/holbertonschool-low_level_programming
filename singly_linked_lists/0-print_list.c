ba4abf1bbb3474b6a0fb37a04b4e038887a62b94
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-03 10:37:43 -0400

    Add function that prints elements of sll

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include "lists.h"
+#include <stdio.h>
+
+/** print_list - prints all the elements of a list
+ * @h: head of the linked list
+ * Return: Number of elements in the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *p = h;
+	size_t i = 0;
+
+	while (p)
+	{
+		if (p->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", p->len, p->str);
+
+		p = p->next;
+		++i;
+	}
+
+	return (i);
+}
