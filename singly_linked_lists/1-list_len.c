dc12afaaf24e7a6db183816e54b4220e59d3120b
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-04 09:20:44 -0400

    Correct betty format

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,21 +1,20 @@
 #include <stddef.h>
 #include "lists.h"
 
-/** 
+/**
  * list_len - Gets the length of the singly linked list
- * @h - head of the linked list
+ * @h: head of the linked list
  * Return: Number of elements in the sll
  */
 size_t list_len(const list_t *h)
 {
 	size_t len = 0;
-	int i;
 	const list_t *p = h;
 
-	for(i = 0; p; i++)
+	while (p)
 	{
-		p = p->next;
 		len++;
+		p = p->next;
 	}
 	return (len);
 }
