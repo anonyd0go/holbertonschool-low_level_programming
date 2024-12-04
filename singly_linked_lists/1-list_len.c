68991c10c2191349125cf1189335fc3cf020c9b1
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-04 09:13:26 -0400

    Correct betty format

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,22 +1,20 @@
 #include <stddef.h>
 #include "lists.h"
 
-/** list_len - Gets the length of the singly linked list
+/** 
+ * list_len - Gets the length of the singly linked list
  * @h - head of the linked list
  * Return: Number of elements in the sll
  */
 size_t list_len(const list_t *h)
 {
 	const list_t *p = h;
-	size_t i = 1;
+	size_t i = 0;
 
-	if (p == NULL)
-		return (0);
-	else if (p->next == NULL)
-		return (1);
-	else
+	while (p)
 	{
+		i++;
 		p = p->next;
-		return (i + list_len(p));
 	}
+	return (i);
 }
