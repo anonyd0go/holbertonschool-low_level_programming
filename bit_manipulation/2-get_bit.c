e468b0c76d1a44233b1a1bb75116e5b9e6654869
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-07 10:55:48 -0400

    Add function that finds the bit at an index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_but - gets the value of an bit in a given index
+ * @n: Unsigned int
+ * @index: index to find the bit
+ * Return: bit at the desired index, -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (sizeof(n) * 8 < index)
+		return (-1);
+
+	n >>= index;
+	if ((n & 1) == 1)
+		return (1);
+	else
+		return (0);
+}
